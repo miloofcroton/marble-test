c0f5981e67c65cf10bb405f62c6c9202e0eedd77
Jack Toumey <jack@toumey.io>
2019-08-09 21:08:28 -0700

    tests fixed

--- src/resources/users/tests/effects.test.ts
+++ src/resources/users/tests/effects.test.ts
@@ -6,9 +6,40 @@ import { mockUser } from '../../users/mocks';
 import { UsersMethods } from '../methods';
 import httpListener from '../../../app';
 
-const app = httpListener.run(createContext());
+describe('getUserListEffect$', () => {
+  const app = httpListener.run(createContext());
+
+  test('GET /api/v1/users/ returns 200 status and list of users', async () => {
+    const users = [await mockUser(), await mockUser()];
+    const token = await mockAuthorizationFor(users[0])(app);
+
+    return request(app)
+      .get('/api/v1/users')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .then(({ body }) => {
+        users.forEach((user, i) => {
+          const result = body[i];
+          expect(result._id).toEqual(String(user._id));
+          expect(result.firstName).toEqual(user.firstName);
+          expect(result.lastName).toEqual(user.lastName);
+          expect(result.password).toBeUndefined();
+          expect(result.roles).toBeUndefined();
+          expect(result.email).toBeUndefined();
+        });
+      });
+  });
+
+  test('GET /api/v1/users/ returns 401 status when not authorized', async () =>
+    request(app)
+      .get('/api/v1/users')
+      .expect(401, { error: { status: 401, message: 'Unauthorized' } })
+  );
+});
+
 
 describe('getMeEffect$', () => {
+  const app = httpListener.run(createContext());
 
   test('GET /api/v1/users/me returns 200 and currently logged user details', async () => {
     const user = await mockUser();
@@ -49,36 +80,3 @@ describe('getMeEffect$', () => {
   });
 });
 
-describe('getUserListEffect$', () => {
-
-  test('GET /api/v1/users/ returns 200 status and list of users', async () => {
-    const users = [await mockUser(), await mockUser()];
-    const token = await mockAuthorizationFor(users[0])(app);
-
-
-    console.log(users)
-    console.log(token)
-
-    return request(app)
-      .get('/api/v1/users')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(200)
-      .then(({ body }) => {
-        users.forEach((user, i) => {
-          const result = body[i];
-          expect(result._id).toEqual(String(user._id));
-          expect(result.firstName).toEqual(user.firstName);
-          expect(result.lastName).toEqual(user.lastName);
-          expect(result.password).toBeUndefined();
-          expect(result.roles).toBeUndefined();
-          expect(result.email).toBeUndefined();
-        });
-      });
-  });
-
-  test('GET /api/v1/users/ returns 401 status when not authorized', async () =>
-    request(app)
-      .get('/api/v1/users')
-      .expect(401, { error: { status: 401, message: 'Unauthorized' } })
-  );
-});
