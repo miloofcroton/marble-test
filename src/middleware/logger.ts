4cc80f0ae6ae7f2678459f6854139e8a48b67ed1
Jack Toumey <jack@toumey.io>
2019-08-09 20:41:48 -0700

    fixing git issues

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,4 +1,4 @@
 import { logger$ as log$ } from '@marblejs/middleware-logger';
-import { isTestEnv } from '../util/env';
+import { isTestEnv } from '../util/envHelpers';
 
 export const logger$ = log$({ silent: isTestEnv() });
