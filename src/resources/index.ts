d55b19a57c7d42abc0ef05444c763d1a8b04a6a1
Jack Toumey <jack@toumey.io>
2019-08-10 00:04:47 -0700

    adding spacing for clarity

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,8 +1,10 @@
 import { combineRoutes } from '@marblejs/core';
+
 import { version$ } from './etc/version/routes';
 import { preflight$ } from './etc/preflight/routes';
 import { getFile$ } from './etc/getFiles/routes';
 import { notFound$ } from './etc/notFound/routes';
+
 import { auth$ } from './auth/api/routes';
 import { users$ } from './users/api/routes';
 import { actors$ } from './actors/api/routes';
@@ -10,10 +12,12 @@ import { movies$ } from './movies/api/routes';
 
 export const routes$ = combineRoutes('/api/v1', [
   version$,
+
   auth$,
   users$,
   actors$,
   movies$,
+
   getFile$,
   preflight$,
   notFound$
