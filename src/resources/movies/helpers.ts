2f00cdaeca96f6f7d9f86ef2b9be3aa35e225380
Jack Toumey <jack@toumey.io>
2019-08-09 21:34:58 -0700

    docs cleanup

--- src/resources/movies/helpers.ts
+++ src/resources/movies/helpers.ts
@@ -4,12 +4,12 @@ import { getHostname } from '../../util/http';
 import { CollectionQueryResult } from '../../util/helpers';
 import { Movie } from './models';
 
-export const applyHostnameForCollection = (req: HttpRequest) => (result: CollectionQueryResult<any>) => ({
-  ...result,
-  collection: result.collection.map(applyHostname(req)),
-});
-
 export const applyHostname = (req: HttpRequest) => (movie: InstanceType<Movie>): Movie => ({
   ...movie.toJSON(),
   posterUrl: getHostname(req) + '/api/v1/assets' + movie.posterUrl,
+});
+
+export const applyHostnameForCollection = (req: HttpRequest) => (result: CollectionQueryResult<any>) => ({
+  ...result,
+  collection: result.collection.map(applyHostname(req)),
 });
